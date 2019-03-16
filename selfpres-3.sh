f6f2c38607688bdf95d622b8340f40b05fc2f06a
przemekgorzynski <przemekgorzynski@github.com>
2019-03-16 11:00:41 -0400

    working on script

--- selfpres-3.sh
+++ selfpres-3.sh
@@ -23,18 +23,57 @@
 #====
 #====
 
+#solution
+#first checks if 1st argument is -b. If tyes then we cut first one (shift)  then checks another arguments.   
+
 clear
 ilosc_arg=$#
-defaultp1=p1
-defaultp2=p2
-defaultp3=p3
-
-#  if [[ $1 == "-b" ]]; then shift fi 
+echo $ilosc_arg
 
 set -x
-if [ $ilosc_arg == 0 ]; then
-     #for i in 1 2 3; do echo "==$defaultp$i=="; done
-      for i in 1 2 3; do a="\$defaultp$i" ;echo "to jest A:$a" ; done 
+
+if [[ $1 == "-b" ]];  			#fitst argument = -b
+	then 
+	shift            		# removes first arg -b
+	var_b=1;
 fi
 
+
+if (( $var_b==1 ));
+	then
+		if [ $ilosc_arg = 3 ];
+		then   
+  			p1=$1
+			p3="p3"	
+	 		if [ -z $2 ]; 
+			then p2=""
+			else p2=$p2
+			fi
+		else
+			p1=$1
+			if [ -z $2 ]; 
+ 42                     then p2=""
+ 43                     else p2=$p2
+ 44                     fi
+
+			if [ -z $3 ];
+			then p3=""
+			else p3=$3
+			fi 
+		fi
+fi		
+
+
 set +x
+
+echo "==$p1=="
+echo "==$p2=="
+echo "==$p3=="
+
+#set -x
+#if [ $ilosc_arg == 0 ]; then
+     #for i in 1 2 3; do echo "==$defaultp$i=="; done
+ #     for i in 1 2 3; do a="\$defaultp$i" ;echo "to jest A:$a" ; done 
+#fi
+
+#set +x
