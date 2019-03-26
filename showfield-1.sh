8276a8a537792838e38f37de04b93c49fb804097
przemekgorzynski <przemekgorzynski@github.com>
2019-03-26 05:47:51 -0400

    commiting final script

--- showfield-1.sh
+++ showfield-1.sh
@@ -1,14 +1,18 @@
 #Napisz skrypt showfield-1.sh, który wypisuje w kolejnych wierszach podane pole wskazanego pliku. Załóż, że domyślnie pola rozdzielone są spacją/tabulatorem. Nazwę pliku należy podać jako pierwszy argument wywołania skryptu, a numer pola – jako drugi. Opcja -d <separator> powoduje użycie jako separatora pól podanego znaku (znaków) zamiast spacji/tabulatora.
 
 
-if [ "$1" != "-d:" ]; 
+if [[ "$1" == \-d* ]];
+then separator="true"
+else separator="false"
+fi
+
+
+if [ "$separator" == "false" ]; 
 then 
 	echo "`cut -d " " -f $2 $1`"
 else 
-	separator="`echo $1 | grep -o [[:punct:]] | grep -v "-"`"    #resolving separator
-	echo "separator wynosi $separator"
+	separator1="`echo $1 | grep -o [[:punct:]] | grep -v "-"`"    #resolving separator
+	#echo "separator wynosi $separator1"
 	kolumna=$3
-	echo "kolumna= $kolumna"
-	echo "plik $2"
-	echo "`awk -F$separator '{ print $'$kolumna' }' $2 `" | head -n2  # $'$var'  passing $var value to awk 
+	echo "`awk -F$separator1 '{ print $'$kolumna' }' $2 `" | head -n2  # $'$var'  passing $var value to awk 
 fi
