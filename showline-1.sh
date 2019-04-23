b7c19d4255787fe3eb5b5ac8d8b8be75bc61c3bd
przemekgorzynski <przemekgorzynski@github.com>
2019-04-23 18:05:06 -0400

    commit to test jenkins

--- showline-1.sh
+++ showline-1.sh
@@ -1,7 +1,7 @@
 #Napisz skrypt showline.sh-1, który wypisuje z danego pliku tekstowego (pierwszy argument)wiersz (lub wiersze), którego numer jest określony przez drugi argument (lub kolejne argumenty). Jeśli zostanie użyta opcja -r, to skrypt wyświetla podany wiersz, ale jego numer jest liczony od końca pliku.
 
 #Komendy systemowe: head tail
-
+#add line to test jenkins
 
 if [ "$2" != "-r" ];
 then
@@ -9,3 +9,5 @@ then
 else 
 	tail -n $3 $1 | head -n 1
 fi
+
+
